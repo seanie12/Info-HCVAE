6bc9a57d0162b195bb3ad074062251e3cb7a6ba9
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2022-11-16 11:30:37 +0700

    Update models.py

--- Info-HCVAE/vae/models.py
+++ Info-HCVAE/vae/models.py
@@ -93,9 +93,10 @@ class CategoricalMMDLoss(nn.Module):
     def forward(self, posterior_za_logits, prior_za_logits):
         # input shape = (batch, dim1, dim2)
         batch_size = posterior_za_logits.size(0)
+        num_samples = 50
         # after .unsqueeze(1): (batch, dim1, dim2) -> (batch, 1, dim1, dim2)
-        posterior_zas = gumbel_softmax(posterior_za_logits.unsqueeze(1).repeat(1, 200, 1, 1).view(batch_size*200, -1), hard=True)
-        prior_zas = gumbel_softmax(prior_za_logits.unsqueeze(1).repeat(1, 200, 1, 1).view(batch_size*200, -1), hard=True)
+        posterior_zas = gumbel_softmax(posterior_za_logits.unsqueeze(1).repeat(1, num_samples, 1, 1).view(batch_size*num_samples, -1), hard=True)
+        prior_zas = gumbel_softmax(prior_za_logits.unsqueeze(1).repeat(1, num_samples, 1, 1).view(batch_size*num_samples, -1), hard=True)
         return compute_mmd(prior_zas.view(-1, prior_zas.shape[1]*prior_zas.shape[2]),
                             posterior_zas.view(-1, posterior_zas.shape[1]*posterior_zas.shape[2]))
 
@@ -107,11 +108,12 @@ class GaussianKernelMMDLoss(nn.Module):
     def forward(self, posterior_mu, posterior_logvar, prior_mu, prior_logvar):
         # input shape = (batch, dim)
         batch_size = posterior_mu.size(0)
+        num_samples = 50
         # (batch, dim) -> (batch, 1, dim) after .unsqueeze(1)
-        posterior_zqs = posterior_mu.unsqueeze(1) + torch.randn_like(posterior_mu.unsqueeze(1).repeat(1, 200, 1))*torch.exp(0.5*posterior_logvar.unsqueeze(1))
-        prior_zqs = prior_mu.unsqueeze(1) + torch.randn_like(prior_mu.unsqueeze(1).repeat(1, 200, 1))*torch.exp(0.5*prior_logvar.unsqueeze(1))
+        posterior_zqs = posterior_mu.unsqueeze(1) + torch.randn_like(posterior_mu.unsqueeze(1).repeat(1, num_samples, 1))*torch.exp(0.5*posterior_logvar.unsqueeze(1))
+        prior_zqs = prior_mu.unsqueeze(1) + torch.randn_like(prior_mu.unsqueeze(1).repeat(1, num_samples, 1))*torch.exp(0.5*prior_logvar.unsqueeze(1))
         # result tensor shape = (batch, 200, dim)
-        return compute_mmd(prior_zqs.view(batch_size*200, -1), posterior_zqs.view(batch_size*200, -1))
+        return compute_mmd(prior_zqs.view(batch_size*num_samples, -1), posterior_zqs.view(batch_size*num_samples, -1))
 
 
 class Embedding(nn.Module):
