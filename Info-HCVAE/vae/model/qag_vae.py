c77ce80d11c4cd93db5545b9ef306b4c3585c0f0
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2022-11-26 19:31:58 +0700

    update

--- Info-HCVAE/vae/model/qag_vae.py
+++ Info-HCVAE/vae/model/qag_vae.py
@@ -220,6 +220,7 @@ class DiscreteVAE(nn.Module):
         for param in self.prior_za_info.parameters():
             param.requires_grad = False
 
+        # Get params
         params = filter(lambda p: p.requires_grad, self.parameters())
 
         # Restore requires_grad
