891725482c07a6ee14dad90a1634626052c0288f
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2022-12-09 21:30:06 +0700

    Update trainer.py

--- Info-HCVAE/vae/trainer.py
+++ Info-HCVAE/vae/trainer.py
@@ -9,8 +9,8 @@ class VAETrainer(object):
         self.device = args.device
 
         self.vae = DiscreteVAE(args).to(self.device)
-        # self.params = filter(lambda p: p.requires_grad, self.vae.parameters())
-        self.params = self.vae.get_vae_params(lr=args.lr) + (self.vae.get_infomax_params(lr=3e-4) if args.lambda_z_info > 0 else [])
+        self.params = filter(lambda p: p.requires_grad, self.vae.parameters())
+        # self.params = self.vae.get_vae_params(lr=args.lr) + (self.vae.get_infomax_params(lr=3e-4) if args.lambda_z_info > 0 else [])
         if args.use_sgd:
             self.optimizer = torch.optim.SGD(self.params, lr=args.lr, momentum=0.9, nesterov=True, weight_decay=args.weight_decay)
         else:
@@ -58,7 +58,7 @@ class VAETrainer(object):
         if self.cnt_steps % 100 == 0:
             self.print_log()
 
-        self.adjust_infomax_weight(return_dict["loss_z_info"].item())
+        # self.adjust_infomax_weight(return_dict["loss_z_info"].item())
 
 
     def change_optimizer(self, optimizer="adam", lr=1e-4, weight_decay=0.0):
