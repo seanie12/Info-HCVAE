4b003ffec156dc4344d4651bebd1452d5dbd56a3
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2022-12-09 17:05:35 +0700

    Update trainer.py

--- Info-HCVAE/vae/trainer.py
+++ Info-HCVAE/vae/trainer.py
@@ -9,8 +9,8 @@ class VAETrainer(object):
         self.device = args.device
 
         self.vae = DiscreteVAE(args).to(self.device)
-        self.params = filter(lambda p: p.requires_grad, self.vae.parameters())
-        # self.params = self.vae.get_vae_params(lr=args.lr) + (self.vae.get_infomax_params(lr=args.lr/10) if args.lambda_z_info > 0 else [])
+        # self.params = filter(lambda p: p.requires_grad, self.vae.parameters())
+        self.params = self.vae.get_vae_params(lr=args.lr) + (self.vae.get_infomax_params(lr=args.lr/100) if args.lambda_z_info > 0 else [])
         if args.use_sgd:
             self.optimizer = torch.optim.SGD(self.params, lr=args.lr, momentum=0.9, nesterov=True, weight_decay=args.weight_decay)
         else:
