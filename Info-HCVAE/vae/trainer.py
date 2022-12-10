f27eaf6f57527dec2de7794d3ed1ab1fdbca4255
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2022-12-10 23:20:01 +0700

    Update trainer.py

--- Info-HCVAE/vae/trainer.py
+++ Info-HCVAE/vae/trainer.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from model.qag_vae import DiscreteVAE
 
+
 class VAETrainer(object):
     def __init__(self, args):
         self.args = args
@@ -12,9 +13,11 @@ class VAETrainer(object):
         self.params = filter(lambda p: p.requires_grad, self.vae.parameters())
         # self.params = self.vae.get_vae_params(lr=args.lr) + (self.vae.get_infomax_params(lr=args.lr/100) if args.lambda_z_info > 0 else [])
         if args.use_sgd:
-            self.optimizer = torch.optim.SGD(self.params, lr=args.lr, momentum=0.9, nesterov=True, weight_decay=args.weight_decay)
+            self.optimizer = torch.optim.SGD(
+                self.params, lr=args.lr, momentum=0.9, nesterov=True, weight_decay=args.weight_decay)
         else:
-            self.optimizer = torch.optim.Adam(self.params, lr=args.lr, weight_decay=args.weight_decay)
+            self.optimizer = torch.optim.Adam(
+                self.params, lr=args.lr, weight_decay=args.weight_decay)
 
         self.losses = {
             "total_loss": 0,
@@ -23,27 +26,21 @@ class VAETrainer(object):
             "loss_kl": 0,
             "loss_zq_kl": 0,
             "loss_za_kl": 0,
-            "loss_mmd": 0,
-            "loss_zq_mmd": 0,
-            "loss_za_mmd": 0,
-            "loss_z_info": 0,
-            "loss_zq_info": 0,
-            "loss_za_info": 0,
+            "loss_span_info": 0,
             "loss_qa_info": 0,
         }
         self.cnt_steps = 0
-
 
     def adjust_infomax_weight(self, infomax_loss):
         if infomax_loss > 1.0:
             self.vae.reduce_infomax_weight_by_10()
 
-
     def train(self, c_ids, q_ids, a_ids, start_positions, end_positions):
         self.vae.train()
 
         # Forward
-        return_dict = self.vae(c_ids, q_ids, a_ids, start_positions, end_positions)
+        return_dict = self.vae(c_ids, q_ids, a_ids,
+                               start_positions, end_positions)
 
         # Backward
         self.optimizer.zero_grad()
@@ -60,16 +57,16 @@ class VAETrainer(object):
 
         # self.adjust_infomax_weight(return_dict["loss_z_info"].item())
 
-
     def change_optimizer(self, args, optimizer="adam", lr=1e-4, weight_decay=0.0):
         assert optimizer in ["sgd", "adam"]
         self.params = filter(lambda p: p.requires_grad, self.vae.parameters())
         # self.params = self.vae.get_vae_params(lr=lr) + (self.vae.get_infomax_params(lr=lr/100) if args.lambda_z_info > 0 else [])
         if optimizer == "sgd":
-            self.optimizer = torch.optim.SGD(self.params, lr=lr, momentum=0.9, nesterov=True, weight_decay=weight_decay)
+            self.optimizer = torch.optim.SGD(
+                self.params, lr=lr, momentum=0.9, nesterov=True, weight_decay=weight_decay)
         else:
-            self.optimizer = torch.optim.Adam(self.params, lr=lr, weight_decay=weight_decay)
-
+            self.optimizer = torch.optim.Adam(
+                self.params, lr=lr, weight_decay=weight_decay)
 
     def print_log(self, log_type="step", epoch=None):
         """
@@ -77,7 +74,8 @@ class VAETrainer(object):
             epoch: enter a number
         """
         assert log_type in ["step", "epoch"]
-        assert log_type == "step" or (log_type == "epoch" and epoch is not None)
+        assert log_type == "step" or (
+            log_type == "epoch" and epoch is not None)
 
         log_str = ""
         if log_type == "step":
@@ -86,41 +84,38 @@ class VAETrainer(object):
             log_str = "\nEpoch={:d} - ".format(epoch)
 
         for key, value in self.losses.items():
-            log_str += "{:s}={:.4f};".format(key, value / self.cnt_steps)
+            log_str += "{:s}={:.4f}; ".format(key, value / self.cnt_steps)
 
         print(log_str + "\n")
-
 
     def _reset_loss_values(self):
         for key in self.losses.keys():
             self.losses[key] = 0
 
-
     def reset_cnt_steps(self):
         self.cnt_steps = 0
         self._reset_loss_values()
 
-
     def generate_posterior(self, c_ids, q_ids, a_ids):
         with torch.no_grad():
             zq, za = self.vae.posterior_encoder(c_ids, q_ids, a_ids)
-            q_ids, start_positions, end_positions = self.vae.generate(zq, za, c_ids)
+            q_ids, start_positions, end_positions = self.vae.generate(
+                zq, za, c_ids)
         return q_ids, start_positions, end_positions, zq
-
 
     def generate_answer_logits(self, c_ids, q_ids, a_ids):
         with torch.no_grad():
             zq, za = self.vae.posterior_encoder(c_ids, q_ids, a_ids)
-            start_logits, end_logits = self.vae.return_answer_logits(zq, za, c_ids)
+            start_logits, end_logits = self.vae.return_answer_logits(
+                zq, za, c_ids)
         return start_logits, end_logits
-
 
     def generate_prior(self, c_ids):
         with torch.no_grad():
             zq, za = self.vae.prior_encoder(c_ids)
-            q_ids, start_positions, end_positions = self.vae.generate(zq, za, c_ids)
-        return q_ids, start_positions, end_positions, zq    
-
+            q_ids, start_positions, end_positions = self.vae.generate(
+                zq, za, c_ids)
+        return q_ids, start_positions, end_positions, zq
 
     def save(self, filename):
         params = {
@@ -129,11 +124,9 @@ class VAETrainer(object):
         }
         torch.save(params, filename)
 
-
     def load_model_state_dict(self, filename):
         params = torch.load(filename)
         self.vae.load_state_dict(params["vae_state_dict"])
-
 
     def set_eval_mode(self, enable=True):
         if enable:
