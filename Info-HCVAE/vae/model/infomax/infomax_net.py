8e558c202af663a8c26e9b6e469ea3d8d7d0b713
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2022-12-10 07:28:21 +0700

    maximize w.r.t mean emb

--- Info-HCVAE/vae/model/infomax/infomax_net.py
+++ Info-HCVAE/vae/model/infomax/infomax_net.py
@@ -6,11 +6,9 @@ from .dim_bce_infomax import DimBceInfoMax
 
 
 class LatentDimMutualInfoMax(nn.Module):
-    def __init__(self, emsize, max_seq_len, max_question_len, nzqdim, nza, nzadim, infomax_type="deep"):
+    def __init__(self, emsize, nzqdim, nza, nzadim, infomax_type="deep"):
         super(LatentDimMutualInfoMax, self).__init__()
         assert infomax_type in ["deep", "bce"]
-        self.max_seq_len = max_seq_len
-        self.max_question_len = max_question_len
         self.emsize = emsize
         self.nzqdim = nzqdim
         self.nza = nza
@@ -19,25 +17,25 @@ class LatentDimMutualInfoMax(nn.Module):
 
         if infomax_type == "deep":
             self.zq_infomax = MineInfoMax(
-                x_dim=emsize*(max_seq_len+max_question_len), z_dim=nzqdim)
+                x_dim=emsize*2, z_dim=nzqdim)
             self.za_infomax = MineInfoMax(
-                x_dim=emsize*max_seq_len + nzqdim, z_dim=nza*nzadim)
+                x_dim=emsize + nzqdim, z_dim=nza*nzadim)
         elif infomax_type == "bce":
-            self.zq_infomax = DimBceInfoMax(x_dim=emsize*(max_seq_len+max_question_len), z_dim=nzqdim)
-            self.za_infomax = DimBceInfoMax(x_dim=emsize*max_seq_len + nzqdim, z_dim=nza*nzadim, linear_bias=False)
+            self.zq_infomax = DimBceInfoMax(x_dim=emsize*2, z_dim=nzqdim)
+            self.za_infomax = DimBceInfoMax(x_dim=emsize + nzqdim, z_dim=nza*nzadim, linear_bias=False)
 
     def forward(self, zq, za, c_f, c_a_f=None, q_f=None):
         N, _, _ = c_f.size()
 
         if q_f is not None:
-            x_zq = torch.cat([q_f, c_f], dim=1).view(N, -1)
+            x_zq = torch.cat([q_f.mean(dim=1), c_f.mean(dim=1)], dim=1).view(N, -1)
         else:
-            x_zq = c_f.view(N, -1)
+            x_zq = c_f.mean(dim=1).view(N, -1)
 
         if c_a_f is not None:
-            x_za = torch.cat([zq, c_a_f.view(N, -1)], dim=1)
+            x_za = torch.cat([zq, c_a_f.mean(dim=1).view(N, -1)], dim=1)
         else:
-            x_za = torch.cat([zq, c_f.view(N, -1)], dim=1)
+            x_za = torch.cat([zq, c_f.mean(dim=1).view(N, -1)], dim=1)
 
         return self.zq_infomax(x_zq, zq), self.za_infomax(x_za, za.view(N, -1))
 
